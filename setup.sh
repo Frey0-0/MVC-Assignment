becebd590865a2e8219df96940c734b44f3ac2e0
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2022-07-26 01:06:46 +0530

    makes password invisible and some other improvements

--- setup.sh
+++ setup.sh
@@ -33,15 +33,26 @@ else
     read DB_USERNAME
     echo '$DB_USERNAME= "'$DB_USERNAME'";' >>config.php
     echo "Enter the password"
-    read DB_PASSWORD
+    read -s DB_PASSWORD
     echo '$DB_PASSWORD= "'$DB_PASSWORD'";' >>config.php
     echo "?>" >>config.php
 
     cd ..
     mysql -u $DB_USERNAME -p$DB_PASSWORD -e "create database $DB_NAME"
-    mysql -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME <schema/schema.sql
 
-    echo "Starting server at port 5000"
-    cd public
-    php -S localhost:5000
+    if [ $? -eq 0 ]; then
+        mysql -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME <schema/schema.sql
+        if [ $? -eq 0 ]; then
+            echo "Starting server at port 5000"
+            cd public
+            php -S localhost:5000
+        else
+            echo "Invalid username or password for the database connection"
+            rm config/config.php
+        fi
+
+    else
+        echo "Invalid username or password for the database connection or the database already exists on your mysql server"
+        rm config/config.php
+    fi
 fi
